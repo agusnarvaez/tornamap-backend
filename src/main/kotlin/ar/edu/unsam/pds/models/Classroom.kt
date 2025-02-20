b759abd0838825817b563dc6de580219287aaed8
Francisco Coronel <ffranncoronel@gmail.com>
2025-02-20 18:59:13 -0300

    Agregamos modelos de Classroom y Building. También necesitamos agregar dos enums: ClassroomType y Floor para poder delimitar los tipos de aula y pisos en los que se pueden encontrar.

--- src/main/kotlin/ar/edu/unsam/pds/models/Classroom.kt
+++ src/main/kotlin/ar/edu/unsam/pds/models/Classroom.kt
@@ -1,4 +1,29 @@
-package ar.edu.unsam.pds.models;
+package ar.edu.unsam.pds.models
 
-public class Classroom {
+import ar.edu.unsam.pds.models.enums.ClassroomType
+import ar.edu.unsam.pds.models.enums.Floor
+import jakarta.persistence.*
+import java.io.Serializable
+import java.util.UUID
+
+@Entity @Table(name = "APP_CLASSROOM")
+class Classroom(
+    val name: String,
+    val type: ClassroomType,
+    val capacity: Int,
+    val floor: Floor,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "building_id", nullable = false)
+    val building: Building
+    ): Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    lateinit var id: UUID
+
+
+/*    init {
+        require(floor in setOf(-1, 0, 1)) { "floor solo puede ser -1, 0 o 1" }
+    }
+APLICAMOS UN ENUM PERO TAMBIÉN TENEMOS ESTA IMPLEMENTACIÓN, HAY ALGUNA RECOMENDADA? */
 }
---
+++ src/main/kotlin/ar/edu/unsam/pds/models/enums/Floor.kt
@@ -0,0 +1,5 @@
+package ar.edu.unsam.pds.models.enums
+
+enum class Floor(val value: Int) {
+    BASEMENT(-1), GROUND(0), FIRST(1);
+}
