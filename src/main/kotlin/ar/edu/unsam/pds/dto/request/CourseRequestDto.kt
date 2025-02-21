8df66bdc6eaf7db00b8924a3d1363f8066691268
juaanicl <juanicluna@gmail.com>
2025-02-21 19:25:49 -0300

    se ajusta service de course

--- src/main/kotlin/ar/edu/unsam/pds/dto/request/CourseRequestDto.kt
+++ src/main/kotlin/ar/edu/unsam/pds/dto/request/CourseRequestDto.kt
@@ -23,5 +23,5 @@ data class CourseRequestDto(
         regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
         message = "UUID debe ser valido"
     )
-    var id: String?
+    var programId: String?
 )
