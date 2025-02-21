8ebf918d7dbf8c8aae820a1f6bbb4b3363bf4de3
juaanicl <juanicluna@gmail.com>
2025-02-21 15:50:40 -0300

    Se adapto la clase course

--- src/main/kotlin/ar/edu/unsam/pds/models/Course.kt
+++ src/main/kotlin/ar/edu/unsam/pds/models/Course.kt
@@ -5,61 +5,17 @@ import java.io.Serializable
 import java.util.*
 
 @Entity @Table(name = "APP_COURSE")
-class Course(
+class
+
+Course(
     val title: String,
 
     @Column(length = 1024)
     val description: String,
 
-    var category: String,
     @Column(length = 1024)
     var image: String
 ) : Timestamp(), Serializable {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     lateinit var id: UUID
-
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "course", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val assignments = mutableSetOf<Assignment>()
-
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "course", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val reviews= mutableSetOf<Review>()
-
-    fun addAssignment(assignment: Assignment) {
-        assignments.add(assignment)
-        assignment.attachCourse(this)
-    }
-
-    fun removeAssignment(assignment: Assignment) {
-        assignments.removeIf{ it.id == assignment.id }
-    }
-
-    fun totalIncome(): Double {
-        return assignments.sumOf { it.totalIncome() }
-    }
-
-    fun mostPopularAssignment(): Assignment {
-        return assignments.maxByOrNull { it.subscribedUsers.size }!!
-    }
-
-    fun mostProfitableAssignment(): Assignment {
-        return assignments.maxByOrNull { it.totalIncome() }!!
-    }
-
-    fun totalSubscribedUsers(): Int {
-        return assignments.sumOf { it.totalSubscribedUsers() }
-    }
-
-    fun assigmentsNames(): Set<String> {
-        return assignments.map { it.name() }.toSet()
-    }
-
-    fun averageRating(): Double {
-        if (reviews.isEmpty()) {
-            return 0.0
-        }
-
-        val sum = reviews.sumOf { it.rating.toDouble() }
-        return sum / reviews.size
-    }
-
 }
