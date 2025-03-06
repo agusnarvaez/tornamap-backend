a7fd517bb782efe1653c7e1d857da313a6d2a7f6
juaanicl <juanicluna@gmail.com>
2025-03-06 14:33:17 -0300

    se crea modelo period

---
+++ src/main/kotlin/ar/edu/unsam/pds/models/Period.kt
@@ -0,0 +1,17 @@
+package ar.edu.unsam.pds.models
+
+import jakarta.persistence.*
+import java.io.Serializable
+import java.time.LocalDate
+import java.util.*
+
+@Entity @Table(name = "APP_PERIOD")
+class Period(
+    startDate: LocalDate,
+    endDate: LocalDate,
+): Timestamp(),Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    lateinit var id: UUID
+}
