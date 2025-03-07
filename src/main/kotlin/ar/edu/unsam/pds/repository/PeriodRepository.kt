a185ada1c9bd510bfa185aebfa694de40d4d3238
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-03-07 16:53:50 -0300

    Creación Repositorio-Period

--- src/main/kotlin/ar/edu/unsam/pds/repository/PeriodRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/PeriodRepository.kt
@@ -6,4 +6,42 @@ import java.util.*
 
 
 interface PeriodRepository: JpaRepository<Period, UUID> {
+    @Query("""
+        SELECT p FROM Period p
+        WHERE p.title LIKE concat('%', :query, '%')
+        OR p.description LIKE concat('%', :query, '%')
+        ORDER BY 
+        CASE 
+            WHEN p.title LIKE concat('%', :query, '%') THEN 1
+            ELSE 3
+        END
+    """)
+    fun getAllBy(@Param("query") query: String): MutableList<Period>
+
+    @Query("""
+    SELECT periods FROM Program i
+    JOIN i.periods periods
+    JOIN i.admin admins
+    WHERE admins.id = :#{#principal.user.id}
+    AND (periods.title LIKE concat('%', :query, '%') 
+         OR periods.description LIKE concat('%', :query, '%'))
+    ORDER BY 
+        CASE 
+            WHEN periods.title LIKE concat('%', :query, '%') THEN 1
+            ELSE 3
+        END
+    """
+    )
+    fun getAllByPrincipal(@Param("query")query: String, @Param("principal") principal: Principal): MutableList<Period>
+
+    @Query(
+        """
+        SELECT COUNT(periods.id) = 1
+        FROM Program i
+        JOIN i.periods periods
+        JOIN i.admin admins
+        WHERE periods.id = :idPeriod AND admins.id = :#{#principal.user.id}
+    """
+    )
+    fun isOwner(@Param("idPeriod") idPeriod: UUID, @Param("principal") principal: Principal): Boolean
 }
