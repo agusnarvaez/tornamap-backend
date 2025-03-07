67ff0bc924962317e3dfcb22ea26e374a9c9d50f
juaanicl <juanicluna@gmail.com>
2025-03-07 17:08:51 -0300

    cambios en el repositorio

--- src/main/kotlin/ar/edu/unsam/pds/repository/PeriodRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/PeriodRepository.kt
@@ -2,45 +2,40 @@ package ar.edu.unsam.pds.repository
 
 import ar.edu.unsam.pds.models.Period
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import ar.edu.unsam.pds.security.models.Principal
 import java.util.*
 
 
 interface PeriodRepository: JpaRepository<Period, UUID> {
     @Query("""
         SELECT p FROM Period p
-        WHERE p.title LIKE concat('%', :query, '%')
-        OR p.description LIKE concat('%', :query, '%')
-        ORDER BY 
-        CASE 
-            WHEN p.title LIKE concat('%', :query, '%') THEN 1
-            ELSE 3
-        END
+        WHERE  FUNCTION('TEXT',p.startDate) LIKE concat('%', :query, '%')
+        ORDER BY FUNCTION('TEXT',p.startDate)
     """)
     fun getAllBy(@Param("query") query: String): MutableList<Period>
 
     @Query("""
-    SELECT periods FROM Program i
-    JOIN i.periods periods
+    SELECT period FROM Event i
+    JOIN i.period period
     JOIN i.admin admins
     WHERE admins.id = :#{#principal.user.id}
-    AND (periods.title LIKE concat('%', :query, '%') 
-         OR periods.description LIKE concat('%', :query, '%'))
-    ORDER BY 
-        CASE 
-            WHEN periods.title LIKE concat('%', :query, '%') THEN 1
-            ELSE 3
-        END
+    AND (
+           FUNCTION('TEXT', period.startDate) LIKE concat('%', :query, '%')
+           )
+    ORDER BY FUNCTION('TEXT',period.startDate)
     """
     )
     fun getAllByPrincipal(@Param("query")query: String, @Param("principal") principal: Principal): MutableList<Period>
 
     @Query(
         """
-        SELECT COUNT(periods.id) = 1
-        FROM Program i
-        JOIN i.periods periods
+        SELECT COUNT(period.id) = 1
+        FROM Event i
+        JOIN i.period period
         JOIN i.admin admins
-        WHERE periods.id = :idPeriod AND admins.id = :#{#principal.user.id}
+        WHERE period.id = :idPeriod AND admins.id = :#{#principal.user.id}
     """
     )
     fun isOwner(@Param("idPeriod") idPeriod: UUID, @Param("principal") principal: Principal): Boolean
