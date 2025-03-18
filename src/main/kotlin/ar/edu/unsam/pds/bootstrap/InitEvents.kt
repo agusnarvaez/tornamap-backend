6eb2098400cff0fb7b6137b5f4dc8e006c623679
Francisco Coronel <ffranncoronel@gmail.com>
2025-03-18 18:30:26 -0300

    Quito referencia a schedule de constructores, modifico nombre del bean e inyecto dependencia

--- src/main/kotlin/ar/edu/unsam/pds/bootstrap/InitEvents.kt
+++ src/main/kotlin/ar/edu/unsam/pds/bootstrap/InitEvents.kt
@@ -4,31 +4,31 @@ import ar.edu.unsam.pds.models.Event
 import ar.edu.unsam.pds.models.Course
 import ar.edu.unsam.pds.models.Schedule
 import ar.edu.unsam.pds.repository.EventRepository
-import ar.edu.unsam.pds.repository.InstitutionRepository
+import ar.edu.unsam.pds.repository.ProgramRepository
 import ar.edu.unsam.pds.repository.ScheduleRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.DependsOn
 import org.springframework.stereotype.Component
+import ar.edu.unsam.pds.exceptions.NotFoundException
 
 @Component(value = "InitAssignments.beanName")
 @DependsOn(value = ["InitCourses.beanName", "InitSchedules.beanName"])
+@Component(value = "InitEvents.beanName")
 class InitEvents : BootstrapGeneric("Events") {
     @Autowired private lateinit var scheduleRepository: ScheduleRepository
-    @Autowired private lateinit var institutionRepository: InstitutionRepository
+    @Autowired private lateinit var programRepository: ProgramRepository
     @Autowired private lateinit var eventRepository: EventRepository
 
     override fun doAfterPropertiesSet() {
         // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         // Estrellas en Movimiento + Ballet Clásico para Principiantes #################################################
-        val course11 = this.findByNameAndCourseTitle(
-            name = "Redes Locales",
-            title = "ESTO NO IRIA, BORRAR EN OTRA RAMA"
+        val course11 = this.findByName(
+            name = "Matematica I"
         )
 
         val event111 = Event(
             name = "Parcial",
-            isApproved = true,
-            schedule = findRandomSchedule()
+            isApproved = true
         )
 
         course11?.addEvent(event111)
@@ -36,8 +36,7 @@ class InitEvents : BootstrapGeneric("Events") {
 
         val event112 = Event(
             name = "Final",
-            isApproved = true,
-            schedule = findRandomSchedule()
+            isApproved = true
         )
 
         course11?.addEvent(event112)
@@ -45,8 +44,7 @@ class InitEvents : BootstrapGeneric("Events") {
 
         val event113 = Event(
             name = "Recuperatorio",
-            isApproved = true,
-            schedule = findRandomSchedule()
+            isApproved = true
         )
 
         course11?.addEvent(event113)
