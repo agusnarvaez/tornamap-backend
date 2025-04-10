67fac0496385c4cddb10275f0f743ddd98a46957
Francisco Coronel <ffranncoronel@gmail.com>
2025-04-10 19:58:37 -0300

    Intentamos agregar queries para realizar filtrado de eventos

--- src/main/kotlin/ar/edu/unsam/pds/repository/EventRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/EventRepository.kt
@@ -1,14 +1,47 @@
 package ar.edu.unsam.pds.repository
 
 import ar.edu.unsam.pds.models.Event
-import ar.edu.unsam.pds.security.models.Principal
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import java.time.LocalDate
 import java.util.*
 
 @RepositoryRestResource(exported = false)
 interface EventRepository : JpaRepository<Event, UUID> {
     fun findByName(name: String): Optional<Event>
+
+    fun findAllByOrderByNameAsc(): List<Event>
+
+/*
+    fun findByClassroomIDAndDate(
+        @Param("classroomID") classroomID: String,
+        @Param("eventDate") eventDate: LocalDate
+    ): List<Event>
+*/
+
+    @Query("""
+    SELECT e FROM Event e
+    JOIN e.course c
+    JOIN c.programs p
+    LEFT JOIN e.users prof
+    WHERE 
+        (:searchQuery IS NULL OR 
+         LOWER(c.name) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR
+         LOWER(p.name) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR
+         LOWER(prof.name) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR
+         LOWER(prof.lastName) LIKE LOWER(CONCAT('%', :searchQuery, '%')))
+    ORDER BY c.name ASC
+""")
+    fun findByQuery(
+        @Param("searchQuery") searchQuery: String
+    ): List<Event>
+//    {
+//        this.findByName(searchQuery)
+//        this.findByProfessor(searchQuery)
+//        this.findByCourse(searchQuery)
+//        courseService.findProgram(searchQuery)
+//    }
+
 }
