e6a5b456030315577760d9efe66aad0390fd6c96
agusnarvaez <agus.narvaez@outlook.com>
2025-04-12 17:47:52 -0300

    Refactor Schedule

--- src/main/kotlin/ar/edu/unsam/pds/models/Schedule.kt
+++ src/main/kotlin/ar/edu/unsam/pds/models/Schedule.kt
@@ -1,28 +1,21 @@
 package ar.edu.unsam.pds.models
-
-import ar.edu.unsam.pds.models.enums.RecurrenceWeeks
 import jakarta.persistence.*
-import jakarta.validation.constraints.AssertTrue
 import java.io.Serializable
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalTime
-import java.time.temporal.TemporalAdjusters
 import java.util.*
 
 @Entity @Table(name = "APP_SCHEDULE")
 class Schedule(
-    val startTime: LocalTime,
-    val endTime: LocalTime,
-    val weekDay: DayOfWeek?,
-    val date: LocalDate?,
-    val isVirtual: Boolean,
+    var startTime: LocalTime,
+    var endTime: LocalTime,
+    var weekDay: DayOfWeek?,
+    var date: LocalDate?,
+    var isVirtual: Boolean,
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "classroom_id", nullable = true)
-    val classroom: Classroom?,
 
-) : Timestamp(), Serializable {
+    ) : Timestamp(), Serializable {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     lateinit var id: UUID
 
@@ -30,12 +23,7 @@ class Schedule(
     @JoinColumn(name = "event_id", nullable = true)
     lateinit var event: Event
 
-    fun isBeforeEndDate(enteredDate: LocalDate): Boolean {
-        if (date == null) {
-            throw IllegalStateException("No se puede verificar la fecha porque 'date' es nulo en este Schedule")
-        }
-        return enteredDate.isBefore(date) || enteredDate.isEqual(date)
-    }
-
-
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "classroom_id", nullable = true)
+    var classroom: Classroom? = null
 }
