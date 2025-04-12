9407a69594fb9016b72fce2f73ed09712f22985d
agusnarvaez <agus.narvaez@outlook.com>
2025-04-12 17:48:36 -0300

    Refactor CourseController

--- src/main/kotlin/ar/edu/unsam/pds/controllers/CoursesController.kt
+++ src/main/kotlin/ar/edu/unsam/pds/controllers/CoursesController.kt
@@ -1,31 +1,26 @@
 package ar.edu.unsam.pds.controllers
 
-import ar.edu.unsam.pds.dto.request.CourseRequestDto
-import ar.edu.unsam.pds.dto.response.CourseDetailResponseDto
 import ar.edu.unsam.pds.dto.response.CourseResponseDto
-import ar.edu.unsam.pds.dto.response.CourseStatsResponseDto
-import ar.edu.unsam.pds.security.models.Principal
-import ar.edu.unsam.pds.services.CoursesService
+import ar.edu.unsam.pds.mappers.CourseMapper
+import ar.edu.unsam.pds.services.CourseService
 import io.swagger.v3.oas.annotations.Operation
-import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/courses")
 @CrossOrigin("*")
 class CoursesController : UUIDValid() {
-    @Autowired lateinit var courseServices: CoursesService
+    @Autowired lateinit var courseService: CourseService
 
     @GetMapping("")
     @Operation(summary = "Get all courses")
     fun getAll(
         @RequestParam(required = false) query: String?
     ): ResponseEntity<List<CourseResponseDto>> {
-        return ResponseEntity.ok(courseServices.getAll(query ?: ""))
+        return ResponseEntity.ok(courseService.getAll(query ?: ""))
     }
 
     @DeleteMapping("")
@@ -43,9 +38,9 @@ class CoursesController : UUIDValid() {
     @GetMapping("{courseId}")
     @Operation(summary = "Get a course by ID")
     fun getCourse(
-        @PathVariable idCourse: String
+        @PathVariable courseId: String
     ): ResponseEntity<CourseResponseDto> {
-        this.validatedUUID(idCourse)
-        return ResponseEntity.ok(courseServices.findCourseById(idCourse))
+        this.validatedUUID(courseId)
+        return ResponseEntity.ok(CourseMapper.buildCourseDto(courseService.findCourseById(courseId)))
     }
 }
