f5499890fd22a3b8f607d4e14c4b9b81b565cd54
agusnarvaez <agus.narvaez@outlook.com>
2025-04-12 17:42:54 -0300

    Refactor Schedule Mapper

--- src/main/kotlin/ar/edu/unsam/pds/mappers/ScheduleMapper.kt
+++ src/main/kotlin/ar/edu/unsam/pds/mappers/ScheduleMapper.kt
@@ -8,22 +8,24 @@ object ScheduleMapper {
 
     fun buildScheduleDto(schedule: Schedule): ScheduleResponseDto {
         return ScheduleResponseDto(
+            id = schedule.id.toString(),
             startTime = schedule.startTime,
             endTime = schedule.endTime,
             date = schedule.date,
             weekDay = schedule.weekDay,
             isVirtual = schedule.isVirtual,
+            classroom = schedule.classroom?.let { ClassroomMapper.buildClasroomDto(it) }
         )
     }
 
     fun buildSchedule(schedule: ScheduleRequestDto): Schedule {
+
         return Schedule(
             startTime = schedule.startTime,
             endTime = schedule.endTime,
-            date = schedule.date,
             weekDay = schedule.weekDay,
-            isVirtual = schedule.isVirtual,
-            //classroom
+            date = schedule.date,
+            isVirtual = schedule.isVirtual
         )
     }
 }
