eb9e7544a2a0a96bd11e9941338cf492bdbcc60d
agusnarvaez <agus.narvaez@outlook.com>
2025-04-12 17:40:08 -0300

    Creo Classroom Mapper

---
+++ src/main/kotlin/ar/edu/unsam/pds/mappers/ClassroomMapper.kt
@@ -0,0 +1,18 @@
+package ar.edu.unsam.pds.mappers
+
+import ar.edu.unsam.pds.dto.response.ClassroomResponseDto
+import ar.edu.unsam.pds.models.Classroom
+
+object ClassroomMapper {
+
+    fun buildClasroomDto(classroom: Classroom): ClassroomResponseDto{
+        return ClassroomResponseDto(
+            id = classroom.id.toString(),
+            name = classroom.name,
+            capacity = classroom.capacity,
+            type = classroom.type.toString(),
+            floor = classroom.floor,
+            building = BuildingMapper.buildBuildingDto(classroom.building)
+        )
+    }
+}
