0ab51c4a2685ff2d22335f7adf992b274a2e90c1
agusnarvaez <agus.narvaez@outlook.com>
2025-04-13 22:40:39 -0300

    Corrijo warnings

--- src/main/kotlin/ar/edu/unsam/pds/repository/ClassroomRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/ClassroomRepository.kt
@@ -2,7 +2,6 @@ package ar.edu.unsam.pds.repository
 
 import ar.edu.unsam.pds.models.Classroom
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import java.util.*
