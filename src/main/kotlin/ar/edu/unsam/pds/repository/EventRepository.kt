7a9e747654828a0e065b5e4d7d78d130df4258a5
agusnarvaez <agus.narvaez@outlook.com>
2025-04-13 23:06:09 -0300

    Cambio Warnings

--- src/main/kotlin/ar/edu/unsam/pds/repository/EventRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/EventRepository.kt
@@ -1,13 +1,9 @@
 package ar.edu.unsam.pds.repository
 
-import ar.edu.unsam.pds.models.Course
 import ar.edu.unsam.pds.models.Event
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import java.time.LocalDate
 import java.util.*
 
 @RepositoryRestResource(exported = false)
