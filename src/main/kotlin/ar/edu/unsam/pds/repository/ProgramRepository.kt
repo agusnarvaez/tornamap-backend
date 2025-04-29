32589856fc5e41cca5f70ab4a0f2a4a597f022fb
agusnarvaez <agus.narvaez@outlook.com>
2025-04-29 17:33:56 -0300

    Agrego entity graph para que traiga los cursos del program

--- src/main/kotlin/ar/edu/unsam/pds/repository/ProgramRepository.kt
+++ src/main/kotlin/ar/edu/unsam/pds/repository/ProgramRepository.kt
@@ -1,10 +1,8 @@
 package ar.edu.unsam.pds.repository
 
 import ar.edu.unsam.pds.models.Program
-import ar.edu.unsam.pds.security.models.Principal
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import java.util.*
 
@@ -13,4 +11,8 @@ interface ProgramRepository : JpaRepository<Program, UUID> {
     fun findAllByOrderByNameAsc(): MutableList<Program>
 
     fun existsByName(name: String): Boolean
+
+    // En el repositorio
+    @EntityGraph(attributePaths = ["courses"])
+    override fun findAllById(ids: Iterable<UUID>): List<Program>  // Más idiomático en Kotlin
 }
